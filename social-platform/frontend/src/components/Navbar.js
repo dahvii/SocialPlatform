f414540f184b2d6b112f65192c1a100987012b6f
Eric Rasmusson Lindström <eric.rl@me.com>
2019-10-30 15:48:30 +0100

    A small example test with store

--- social-platform/frontend/src/components/Navbar.js
+++ social-platform/frontend/src/components/Navbar.js
@@ -2,12 +2,20 @@ import React from 'react'
 import { Store } from '../utilities/Store';
 
 export default function Navbar() {
-    const {state} = React.useContext(Store);
+    const {state, dispatch} = React.useContext(Store);
+
+
+function test(){
+    dispatch({
+        type: 'TEST',
+        payload: "adsj"
+    })
+}
     return (
         <div>
             <p>Navbar</p>
             <p>{state.test}</p>
-
+            <button onClick={test}>test</button>
         </div>
     )
 }
