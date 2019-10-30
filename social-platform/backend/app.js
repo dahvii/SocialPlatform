a43ed4219cac00c72b57601c27a5f091ce66ebc3
Johan Schultze <Johan.schultze@gmail.com>
2019-10-30 11:18:16 +0100

    Added backend express mongoose

---
+++ social-platform/backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const config = require('./src/config');
+const app = express();
+const {db} = require('./src/loaders');
+const {router} = require('./src/api');
+
+app.use(express.json());
+app.use(express.static('www'));
+app.use('/', router);
+
+async function startWebServer() {
+    app.listen(config.port, () => console.log('Listening on port ' + config.port));     
+}
+
+db.once('open', () => {
+    startWebServer();
+})
---
+++ social-platform/backend/src/api/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const dbModels = {
+    // Require the mongoose model
+
+}
+
+
+
+module.exports = { router };
