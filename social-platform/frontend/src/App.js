453c035ca96928b3e487cf77d18428d946d01caf
Johan Schultze <Johan.schultze@gmail.com>
2019-10-31 13:06:39 +0100

    Showing nav only when logged in

--- social-platform/frontend/src/App.js
+++ social-platform/frontend/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './css/App.css';
-import {BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/BottomNavbar';
 import Login from './views/Login';
 import Feed from './components/Feed';
+import { Store } from './utilities/Store'
 
 function App() {
+  const { state } = React.useContext(Store);
+  console.log(state.isLoggedIn)
+
   return (
     <Router>
       <div className="App">
-        <Navbar />
+        { state.isLoggedIn ? 
+          <Navbar /> : ''
+        }
         <Route exact path="/" component={Feed} />
         <Route path="/login" component={Login} />
       </div>
--- social-platform/frontend/src/utilities/Store.js
+++ social-platform/frontend/src/utilities/Store.js
@@ -3,6 +3,7 @@ import React from 'react';
 export const Store = React.createContext();
 
 const initialState = {
+    isLoggedIn: true,
     test: 'Detta är från store'
 }
 
