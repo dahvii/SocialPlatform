03e76d80f5e668c22d0127591eb352063594cb76
Johan Schultze <Johan.schultze@gmail.com>
2019-11-01 09:38:11 +0100

    Fix for start

--- social-platform/frontend/src/App.js
+++ social-platform/frontend/src/App.js
@@ -21,10 +21,10 @@ function App() {
         { state.isLoggedIn ? 
           <Navbar /> : ''
         }
-        <PrivateRoute exact path="/" component={Feed} isAuthenticated={state.isLoggedIn} redirectPath="/login"/>
-        <PrivateRoute exact path="/swipe" component={Swipe} isAuthenticated={state.isLoggedIn} redirectPath="/login"/>
-        <PrivateRoute exact path="/profile" component={Profile} isAuthenticated={state.isLoggedIn} redirectPath="/login"/>
-        <PrivateRoute exact path="/messages" component={Messages} isAuthenticated={state.isLoggedIn} redirectPath="/login"/>
+        <PrivateRoute exact path="/" component={Feed} isAuthenticated={state.isLoggedIn} redirectPath="/start"/>
+        <PrivateRoute exact path="/swipe" component={Swipe} isAuthenticated={state.isLoggedIn} redirectPath="/start"/>
+        <PrivateRoute exact path="/profile" component={Profile} isAuthenticated={state.isLoggedIn} redirectPath="/start"/>
+        <PrivateRoute exact path="/messages" component={Messages} isAuthenticated={state.isLoggedIn} redirectPath="/start"/>
         <PrivateRoute path="/start" component={Start} isAuthenticated={!state.isLoggedIn} redirectPath="/"/>
         <PrivateRoute path="/register" component={Register} isAuthenticated={!state.isLoggedIn} redirectPath="/"/>
         <PrivateRoute path="/login" component={Login} isAuthenticated={!state.isLoggedIn} redirectPath="/"/>
