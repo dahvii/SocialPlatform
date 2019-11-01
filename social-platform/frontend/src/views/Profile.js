b86ae911e7f4d61f864b6863537c48a0ab97631d
Johan Schultze <Johan.schultze@gmail.com>
2019-11-01 16:29:07 +0100

    Design for profilepage

--- social-platform/frontend/src/views/Profile.js
+++ social-platform/frontend/src/views/Profile.js
@@ -1,9 +1,29 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Button, Carousel } from 'react-bootstrap'
 import { Store } from '../utilities/Store'
+import imageLoader from '../utilities/ImageHandler';
+import '../css/Profile.css'
 
 export default function Profile() {
-    const { dispatch } = React.useContext(Store);
+    const { state, dispatch } = React.useContext(Store);
+    const [images, setImages] = useState([])
+
+    // const images = [
+    //     {
+    //         src: 'https://avatarfiles.alphacoders.com/126/126080.jpg'
+    //     },
+    //     {
+    //         src: 'https://i.pravatar.cc/400?img=4'
+    //     },
+    //     {
+    //         src: 'https://i.pravatar.cc/400?img=5'
+    //     }
+    // ]
+
+    useEffect(() => {
+        const images = imageLoader();
+        setImages(images)
+    }, [])
 
     const logout = async () => {
         let result = await fetch('/api/logout', {
@@ -15,9 +35,38 @@ export default function Profile() {
         }
     }
 
+    const profilePictures = images.map(image => (
+        <Carousel.Item key={image.id}>
+            <img
+                className="d-block w-100"
+                src={image.src}
+                alt="First slider"
+            />
+        </Carousel.Item>
+    ))
+
     return (
         <div>
-            <p>Profile view</p>
+            <Carousel interval={null} fade={true}>
+                {profilePictures}
+            </Carousel>
+            <div className="profile-info">
+                <div className="name-age">
+                    <h3>{state.currentUser.firstName}&nbsp;-</h3 >&nbsp;<h3>25</h3>
+                </div>
+                <div className="town-location">
+                    <div className="hometown">
+                        <i class="fas fa-home"></i>
+                        <p>Malmö</p>
+                    </div>
+                    <div className="location">
+                        <i class="fas fa-map-marker-alt"></i>
+                        <p>5 km</p>
+                    </div>
+                    <hr />
+                    <div className="bio"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus quo eum ipsam reprehenderit saepe nesciunt perferendis repudiandae. Rem quod necessitatibus voluptas. Laudantium sint debitis ad eveniet eum vitae excepturi commodi.</p></div>
+                </div>
+            </div>
             <Button onClick={logout}>Log out</Button>
         </div>
     )
