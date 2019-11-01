f70d15bc4ccfa2697746bafa3be0a6b8ca1bea2f
Johan Schultze <Johan.schultze@gmail.com>
2019-11-01 11:10:02 +0100

    Added loading component

---
+++ social-platform/frontend/src/utilities/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../css/Loading.css'
+
+export default function Loading() {
+    return (
+        <div>
+            <i className="fas fa-bullseye loading button-glow"></i>
+        </div>
+    )
+}
