9e2f72e6218aeeff2a6ad1854682f5db764af171
Unknown <filip98@spray.se>
2019-11-01 13:31:08 +0100

    hämtning av forum post från databasen

--- social-platform/frontend/src/components/FormPost.js
+++ social-platform/frontend/src/components/FormPost.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 export default function FormPost(){
     
@@ -6,18 +6,43 @@ export default function FormPost(){
     const [titel,setTitel] = useState('');
     const [text,setText] = useState('');
     const [comments,setComments] = useState([]);
-    const [timeStamp,setTimeStamp] = useState(new Date(1572529241000));
+    const [timeStamp,setTimeStamp] = useState(new Date(1572602979453));
     const [image,setImage] = useState([]);
+    const [post, setPost] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+
+    const getAllForumPost = async() =>{
+        const data = await fetch('/api/forum');
+        const result = await data.json();
+        setPost(result);
+        setLoading(false) 
+    }
+    useEffect(() => {
+      getAllForumPost();  
+    })
+
     
+
     return(
         <>
-        <Card style={{ width: '18rem' }}>
-            <Card.Body>
-                <Card.Title>{titel}</Card.Title>
-                <Card.Text>Skapad {timeStamp.toLocaleTimeString(undefined, { timeStyle: "short" })}</Card.Text>
-                <Card.Text>{text}</Card.Text>
-            </Card.Body>
-        </Card>
+        {loading ? '': post.forEach(function(post) {
+          log  
+        })
+       
+    }
+        
         </>
     )
 }
+
+/*
+ <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>{post.titel}</Card.Title>
+                <Card.Text>Skapad {timeStamp.toLocaleTimeString(undefined, { timeStyle: "short" })}</Card.Text>
+                <Card.Text>{post.text}</Card.Text>
+            </Card.Body>
+        </Card>
+
+*/
