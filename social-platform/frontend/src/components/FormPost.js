ca689e4f5caf4ca6838fbd3c3760f5cd8298afdd
Unknown <filip98@spray.se>
2019-11-04 13:01:42 +0100

    Forum post och css

--- social-platform/frontend/src/components/FormPost.js
+++ social-platform/frontend/src/components/FormPost.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
+import '../css/ForumPost.css'
 export default function FormPost(){
-    
+
     const [owner,setOwner] = useState('');
     const [titel,setTitel] = useState('');
     const [text,setText] = useState('');
@@ -10,39 +11,36 @@ export default function FormPost(){
     const [image,setImage] = useState([]);
     const [post, setPost] = useState([]);
     const [loading, setLoading] = useState(true);
-
+    const [haveLocktFordata, setHaveLocktFordata] = useState(false);
 
     const getAllForumPost = async() =>{
+        setHaveLocktFordata(true)
+        setLoading(false)
         const data = await fetch('/api/forum');
         const result = await data.json();
-        setPost(result);
-        setLoading(false) 
+        setPost(result); 
     }
-    useEffect(() => {
-      getAllForumPost();  
-    })
-
     
-
-    return(
+    useEffect(() => {
+      if(!haveLocktFordata){
+        getAllForumPost();
+        }
+    })    
+    
+    return(  
         <>
-        {loading ? '': post.forEach(function(post) {
-          log  
-        })
-       
-    }
-        
+        {loading ? '':
+        post.map((post, index) => 
+        <div key={index}>
+             <Card className="Postcard">
+            <Card.Body>
+                <Card.Text className="timestamp">Skapad {timeStamp.toLocaleTimeString(undefined, { timeStyle: "short" })}</Card.Text>
+                <Card.Title className="titel">{post.titel}</Card.Title>
+                <Card.Text>{post.text}</Card.Text>
+            </Card.Body>
+            </Card>
+        </div> 
+            )}
         </>
     )
 }
-
-/*
- <Card style={{ width: '18rem' }}>
-            <Card.Body>
-                <Card.Title>{post.titel}</Card.Title>
-                <Card.Text>Skapad {timeStamp.toLocaleTimeString(undefined, { timeStyle: "short" })}</Card.Text>
-                <Card.Text>{post.text}</Card.Text>
-            </Card.Body>
-        </Card>
-
-*/
