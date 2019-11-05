3a0ad5bf5e5dc34ad1224bf40882f141c1fad7f4
Unknown <filip98@spray.se>
2019-11-05 13:13:30 +0100

    routers till de olika vyerna

--- social-platform/frontend/src/App.js
+++ social-platform/frontend/src/App.js
@@ -11,6 +11,8 @@ import Swipe from './views/Swipe';
 import Messages from './views/Messages';
 import PrivateRoute from './utilities/PrivateRoute'
 import { Store } from './utilities/Store'
+import Forum from './views/Forum'
+import OnePost from './views/OnePost'
 
 function App() {
   const { state } = React.useContext(Store);
@@ -21,13 +23,15 @@ function App() {
         { state.isLoggedIn ? 
           <Navbar /> : ''
         }
-        <PrivateRoute exact path="/" component={Feed} isAuthenticated={state.isLoggedIn} redirectPath="/login"/>
-        <PrivateRoute exact path="/swipe" component={Swipe} isAuthenticated={state.isLoggedIn} redirectPath="/login"/>
-        <PrivateRoute exact path="/profile" component={Profile} isAuthenticated={state.isLoggedIn} redirectPath="/login"/>
-        <PrivateRoute exact path="/messages" component={Messages} isAuthenticated={state.isLoggedIn} redirectPath="/login"/>
+        <PrivateRoute exact path="/" component={Feed} isAuthenticated={state.isLoggedIn} redirectPath="/start"/>
+        <PrivateRoute exact path="/swipe" component={Swipe} isAuthenticated={state.isLoggedIn} redirectPath="/start"/>
+        <PrivateRoute exact path="/profile" component={Profile} isAuthenticated={state.isLoggedIn} redirectPath="/start"/>
+        <PrivateRoute exact path="/messages" component={Messages} isAuthenticated={state.isLoggedIn} redirectPath="/start"/>
         <PrivateRoute path="/start" component={Start} isAuthenticated={!state.isLoggedIn} redirectPath="/"/>
         <PrivateRoute path="/register" component={Register} isAuthenticated={!state.isLoggedIn} redirectPath="/"/>
         <PrivateRoute path="/login" component={Login} isAuthenticated={!state.isLoggedIn} redirectPath="/"/>
+        <PrivateRoute path="/forum" component={Forum} isAuthenticated={state.isLoggedIn} redirectPath="/start"/>
+        <PrivateRoute exact path="/forum/:id" component={OnePost} isAuthenticated={state.isLoggedIn} redirectPath="/start" />
       </div>
     </Router>
   );
