3749e54c68f43f24ff0227c4dd5588039d3c8448
Elliott <dahviimakes@gmail.com>
2019-11-05 13:11:03 +0100

    first draft for like/reject

--- social-platform/frontend/src/components/SwipePreview.js
+++ social-platform/frontend/src/components/SwipePreview.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import Carousel from './Carousel';
 
 export default class SwipePreview extends Component {
 
     render(){
-        console.log(this.props.user);
+       // console.log("swipepreview props.user ",this.props.user);
         
         return (
             <div> 
-                <Carousel></Carousel>
                 <div>{this.props.user ? this.props.user.firstName: "" }</div>
                 <div>Stad</div>
                 <div>icon X km bort</div>
