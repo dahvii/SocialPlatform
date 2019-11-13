0b3e17af8a18152eba81b498d57c92df8a461069
Eric Rasmusson Lindström <eric.rl@me.com>
2019-11-13 13:29:56 +0100

    Some question work

--- social-platform/frontend/src/components/SwipeQuestion.js
+++ social-platform/frontend/src/components/SwipeQuestion.js
@@ -1,30 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Modal, Form } from 'react-bootstrap'
+import { Store } from '../utilities/Store'
+import '../css/SwipeQuestion.css'
 
 export default function SwipeQuestion() {
-    const [show, setShow] = useState(false);
+    const { dispatch } = React.useContext(Store);
+    const { state } = React.useContext(Store);
+    const [checkedGender, setCheckedGender] = useState();
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleGenderOptionChange = (e) => {
+        setCheckedGender(e.target.value)
+    }
+
+    function handleClose() {
+        dispatch({
+            type: "SHOW_QUESTION",
+            payload: false
+        })
+    }
 
     return (
         <>
-            <Button variant="primary" onClick={handleShow}>
-                Launch demo modal
-        </Button>
+            <Modal
+                backdrop="static"
+                show={state.showQuestion}
+                onHide={handleClose}
+                centered>
+                <Modal.Body className="swipe-question-modal">
+                    <Form className="selection swipe-question-form">
+                        <h4 className="swipe-question-question">Fråga</h4>
+                        <div className="edit-profile-form-check">
+                            <label>
+                                <input
+                                    type="radio"
+                                    value="Male"
+                                    checked={checkedGender === "Fråga 1"}
+                                    onChange={handleGenderOptionChange}
+                                    className="form-check-input swipe-question-text"
+                                />
+                                <span className="checkmark"></span>
+                                Fråga1 
+                        </label>
+                        </div>
 
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-            </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-            </Button>
-                </Modal.Footer>
+                        <div className="edit-profile-form-check">
+                            <label className="m-0">
+                                <input
+                                    type="radio"
+                                    value="Female"
+                                    checked={checkedGender === "Fråga 2"}
+                                    onChange={handleGenderOptionChange}
+                                    className="form-check-input"
+                                />
+                                <span className="checkmark"></span>
+                                Fråga 2
+                        </label>
+                        </div>
+                    <Button className="swipe-question-button" variant="light" onClick={handleClose}>
+                        Forstätt
+                    </Button>
+                    </Form>
+                </Modal.Body>
+                
+
             </Modal>
         </>
     );
