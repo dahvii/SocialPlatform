94c2d72991f9144c60ac4aab3e1e706decc4f847
Elliott <dahviimakes@gmail.com>
2019-11-18 16:55:01 +0100

    clean up and return to swipe is working

--- social-platform/backend/src/api/searchAlgorithm.js
+++ social-platform/backend/src/api/searchAlgorithm.js
@@ -4,12 +4,18 @@ const User = require('../models/User')
 
 
 async function getTop10(req, res) {
+    let thisUser = await User.findOne({ _id: req.params.id }).catch();
+    console.log(thisUser);
+
+    //1. ta bort alla användare som redan setts (gör bättre fetch från databas så vi inte hämtar fel personer)
+    //2. skicka tillbaka listan snyggt - nu är det en galen array så vi skickar tbk allusers istället
+    
+    //find users that havent been seen before 
     let allUsers = await User
         .find()
         .populate('myCharacteristics')
         .populate('partnerCharacteristics');
 
-    let thisUser;
 
     allUsers.forEach(user => {
         if (user._id == req.params.id) {
@@ -28,100 +34,62 @@ async function getTop10(req, res) {
     })
 
     //topresult 1
-    let num = 0;
     let sortedUsers = [];
-
     allUsersChar.forEach((user) => {
         if (myPrefSorted[0].color === user[0].color && myPrefSorted[1].color === user[1].color && myPrefSorted[2].color === user[2].color && myPrefSorted[3].color === user[3].color) {
-            num++
             sortedUsers.push(user);
-
         }
     })
-    console.log("första resultat", num);
 
     //result 2
-    num = 0;
-    allUsersChar.forEach((user) => {
-        if (myPrefSorted[0].color === user[0].color && myPrefSorted[1].color === user[1].color && myPrefSorted[2].color === user[2].color) {
-            num++
-            sortedUsers.push(user);
-
-        }
-    })
-    console.log("andra resultat", num);
-
-    //result 3
-    num = 0;
     allUsersChar.forEach((user) => {
         if (myPrefSorted[0].color === user[0].color && myPrefSorted[1].color === user[1].color) {
-            num++
             sortedUsers.push(user);
-
         }
     })
-    console.log("tredje resultat", num);
 
-    //result 4
-    num = 0;
+    //result 3
     allUsersChar.forEach((user) => {
         if (myPrefSorted[0].color === user[0].color) {
-            num++
             sortedUsers.push(user);
-
         }
     })
-    console.log("fjärde resultat", num);
-
-    console.log(sortedUsers);
 
     sortedUsers = sortedUsers.concat(allUsersChar);
-    console.log(sortedUsers.length);
 
     //take away all duplicate
-    /*
-[
-    { value: 7, color: 'yellow' },
-    { value: 2, color: 'green' },
-    { value: 1, color: 'blue' },
-    { value: -6, color: 'red' },
-    { id: 5dd28f128bd55c4d4a956cdb }
-  ],
-  [
-    { value: 3, color: 'yellow' },
-    { value: 1, color: 'green' },
-    { value: -2, color: 'red' },
-    { value: -4, color: 'blue' },
-    { id: 5dd28f128bd55c4d4a956cde }
-  ],
-  [
-    { value: 8, color: 'yellow' },
-    { value: 2, color: 'blue' },
-    { value: 2, color: 'green' },
-    { value: -2, color: 'red' },
-    { id: 5dd28f128bd55c4d4a956ce4 }
-  ],
-    */
-
-
-   console.log("test", sortedUsers[0]);
-   console.log("test", sortedUsers[0][5]);
-
     for (let i = sortedUsers.length - 1; i > 0; i--) {
-        
-        let otherIndex = sortedUsers.findIndex(obj => obj[5] === sortedUsers[i][5]);        
+        let otherIndex = sortedUsers.findIndex(obj => obj[4] === sortedUsers[i][4]);        
         if (otherIndex !== i) {
             sortedUsers.splice(i, 1);
         }
     }
 
-    console.log("99???? ",sortedUsers.length);
+
+    //id fix before sending to frontend
+    let peopleToSwipe = []; 
+    allUsers.forEach( result => {
+        let person = {
+            interests: result.interests,
+            questionsAnswered: result.questionsAnswered,
+            profilePictures: result.profilePictures,
+            likes: result.likes,
+            rejects: result.rejects,
+            matches: result.matches,
+            id: result.id,
+            firstName: result.firstName,
+            email: result.email,
+            gender: result.gender,
+            hometown: result.hometown,
+            bio: result.bio,
+            myCharacteristics: result.myCharacteristics,
+            partnerCharacteristics:result.partnerCharacteristics,
+        }
+        peopleToSwipe.push(person)
+    }); 
     
 
-
-    //console.log("AllUsersChar ",allUsersChar);
-
-
+    res.json(peopleToSwipe)
 
 
 
