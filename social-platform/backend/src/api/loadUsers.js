a80fe3e708dd52109449959ac8fe97621b71c0c7
Eric Rasmusson Lindström <eric.rl@me.com>
2019-11-18 12:22:16 +0100

    Innit users gets characteristics (random value from -10 to 10)

--- social-platform/backend/src/api/loadUsers.js
+++ social-platform/backend/src/api/loadUsers.js
@@ -3,19 +3,29 @@ const data = require('../config/users.json')
 const User = require('../models/User')
 const Characteristics = require('../models/Characteristics')
 
-async function loadUsers(){
+async function loadUsers() {
     await db.collection('users').drop().catch((err) => console.log("no users in db"));
-    
+    await db.collection('characteristics').drop().catch((err) => console.log("no characteristics in db"))
     data.forEach(async user => {
-        const myCharacteristics = await new Characteristics({}).save()
-        const partnerCharacteristics = await new Characteristics({}).save()
+        const myCharacteristics = await new Characteristics({
+            blue: Math.floor(Math.random() * 20) - 10,
+            red: Math.floor(Math.random() * 20) - 10,
+            green: Math.floor(Math.random() * 20) - 10,
+            yellow: Math.floor(Math.random() * 20) - 10
+        }).save()
+        const partnerCharacteristics = await new Characteristics({
+            blue: Math.floor(Math.random() * 20) - 10,
+            red: Math.floor(Math.random() * 20) - 10,
+            green: Math.floor(Math.random() * 20) - 10,
+            yellow: Math.floor(Math.random() * 20) - 10
+        }).save()
         let newUser = new User({
             ...user,
             myCharacteristics,
             partnerCharacteristics
         })
         newUser.save()
-        .catch();
+            .catch();
     })
 }
 
