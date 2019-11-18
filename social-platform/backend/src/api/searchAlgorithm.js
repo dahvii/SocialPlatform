ee0287e4334d4ac142c1102c6dcdbfcb027b97ef
Elliott <dahviimakes@gmail.com>
2019-11-18 18:45:04 +0100

    better find()
    dont fetch users the user already liked/rejected and not them self

--- social-platform/backend/src/api/searchAlgorithm.js
+++ social-platform/backend/src/api/searchAlgorithm.js
@@ -4,62 +4,59 @@ const User = require('../models/User')
 
 
 async function getTop10(req, res) {
-    let thisUser = await User.findOne({ _id: req.params.id }).catch();
-    console.log(thisUser);
-
-    //1. ta bort alla användare som redan setts (gör bättre fetch från databas så vi inte hämtar fel personer)
-    //2. skicka tillbaka listan snyggt - nu är det en galen array så vi skickar tbk allusers istället
+    let thisUser = await User.findOne({ _id: req.params.id }).populate('myCharacteristics').populate('partnerCharacteristics').catch(err => res.status(400));
+    //2. skicka tillbaka den SORTERADE listan snyggt - nu är det en galen array så vi skickar tbk osorterad lista istället
+    //3.skicka bara tbk topp tio - inte hela listan 
     
     //find users that havent been seen before 
-    let allUsers = await User
-        .find()
+    let users = await User.find({ $and:[ 
+            {_id : { $ne: req.params.id} },
+            {_id : { $nin : thisUser.likes} }, 
+            {_id : { $nin : thisUser.rejects} }]            
+        })
         .populate('myCharacteristics')
-        .populate('partnerCharacteristics');
+        .populate('partnerCharacteristics')
+        .catch(err => res.status(400));
+        
+        console.log("listans längd ", users.length);
 
-
-    allUsers.forEach(user => {
-        if (user._id == req.params.id) {
-            thisUser = user;
-            allUsers.splice(allUsers.indexOf(user), 1)
-        }
-    })
     let myPrefSorted = sortPreference(thisUser.partnerCharacteristics);
-    console.log("myPrefSorted ", myPrefSorted);
+    //console.log("myPrefSorted ", myPrefSorted);
 
-    let allUsersChar = [];
-    allUsers.forEach((user) => {
+    let usersChar = [];
+    users.forEach((user) => {
         let userCharSorted = sortPreference(user.partnerCharacteristics);
-        userCharSorted.push({id: user._id});
-        allUsersChar.push(userCharSorted);
+        userCharSorted.push({ id: user._id });
+        usersChar.push(userCharSorted);
     })
 
     //topresult 1
     let sortedUsers = [];
-    allUsersChar.forEach((user) => {
+    usersChar.forEach((user) => {
         if (myPrefSorted[0].color === user[0].color && myPrefSorted[1].color === user[1].color && myPrefSorted[2].color === user[2].color && myPrefSorted[3].color === user[3].color) {
             sortedUsers.push(user);
         }
     })
 
     //result 2
-    allUsersChar.forEach((user) => {
+    usersChar.forEach((user) => {
         if (myPrefSorted[0].color === user[0].color && myPrefSorted[1].color === user[1].color) {
             sortedUsers.push(user);
         }
     })
 
     //result 3
-    allUsersChar.forEach((user) => {
+    usersChar.forEach((user) => {
         if (myPrefSorted[0].color === user[0].color) {
             sortedUsers.push(user);
         }
     })
 
-    sortedUsers = sortedUsers.concat(allUsersChar);
+    sortedUsers = sortedUsers.concat(usersChar);
 
     //take away all duplicate
     for (let i = sortedUsers.length - 1; i > 0; i--) {
-        let otherIndex = sortedUsers.findIndex(obj => obj[4] === sortedUsers[i][4]);        
+        let otherIndex = sortedUsers.findIndex(obj => obj[4] === sortedUsers[i][4]);
         if (otherIndex !== i) {
             sortedUsers.splice(i, 1);
         }
@@ -67,8 +64,8 @@ async function getTop10(req, res) {
 
 
     //id fix before sending to frontend
-    let peopleToSwipe = []; 
-    allUsers.forEach( result => {
+    let peopleToSwipe = [];
+    users.forEach(result => {
         let person = {
             interests: result.interests,
             questionsAnswered: result.questionsAnswered,
@@ -83,41 +80,16 @@ async function getTop10(req, res) {
             hometown: result.hometown,
             bio: result.bio,
             myCharacteristics: result.myCharacteristics,
-            partnerCharacteristics:result.partnerCharacteristics,
+            partnerCharacteristics: result.partnerCharacteristics,
         }
         peopleToSwipe.push(person)
-    }); 
-    
+    });
+
 
     res.json(peopleToSwipe)
-
-
-
-    // .then(result => {
-    //     let idFixedArr = [];
-    //     result.map((user) => {
-    //         const idFixedUser = {
-    //             id: user._id,
-    //             firstName: user.firstName,
-    //             bio: user.bio,
-    //             dateOfBirth: user.dateOfBirth,
-    //             gender: user.gender,
-    //             interests: user.interests,
-    //             matches: user.matches,
-    //             profilePictures: user.profilePictures,
-    //             likes: user.likes,
-    //             rejects: user.rejects
-    //         }
-    //         idFixedArr.push(idFixedUser);
-    //     })
-    //     console.log("innan res")
-    //     res.json(idFixedArr)
-    // })
-    // .catch(err => res.status(400).json('Error: ' + err));
-    ;
 }
 
-function sortPreference(obj) {
+function sortPreference(obj) {    
     let arr = [];
     arr.push({ value: obj.blue, color: "blue" })
     arr.push({ value: obj.yellow, color: "yellow" })
