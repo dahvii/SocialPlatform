b292184c9fbe59332ffb8681d7da348f723c865e
Johan Schultze <Johan.schultze@gmail.com>
2019-11-18 16:29:34 +0100

    Displaying correct profilepictures in swipe carousel

--- social-platform/frontend/src/components/SwipePreview.js
+++ social-platform/frontend/src/components/SwipePreview.js
@@ -9,40 +9,57 @@ export default function Profile(props) {
     const [shortBio, setShortBio] = useState("");
 
     useEffect(() => {
-        if(props.displayedPerson && props.displayedPerson.bio && props.displayedPerson.bio.length > 40){
-            setShortBio(props.displayedPerson.bio.substring(0, 40)+'...');
+        console.log(props.displayedPerson)
+        if (props.displayedPerson && props.displayedPerson.bio && props.displayedPerson.bio.length > 40) {
+            setShortBio(props.displayedPerson.bio.substring(0, 40) + '...');
         }
         //console.log("preview props", props);
-    },[props])
+    }, [props])
 
     useLifeCycle({
         mount: () => {
             const images = imageLoader();
-            setImages(images)    
+            setImages(images)
         }
     })
 
-    const profilePictures = images.map(image => (
-        <Carousel.Item key={image.id}>
-            <img
-                className="d-block carousel-img"
-                src={image.src}
-                alt="First slider"
-            />
-        </Carousel.Item>
-    ))
+    let showProfilePictures;
+
+    if (props.displayedPerson) {
+        showProfilePictures = props.displayedPerson.profilePictures.map(image => (
+            <Carousel.Item key={image}>
+                <img
+                    className="d-block w-100"
+                    src={`http://localhost:3001/${image}`}
+                    alt="First slider"
+                />
+            </Carousel.Item>
+        ))
+    }
 
     return (
         <div>
-            {props.displayedPerson && 
-            <div className="preview-info" onClick={props.changeView}>
-                <div className="name-age">
-                    <h3>{props.displayedPerson.firstName}&nbsp;-</h3 >&nbsp;<h3>25</h3>
-                </div>
-                <div className="bio"><p>{shortBio}</p></div>
-            </div>}
+            {props.displayedPerson &&
+                <div className="preview-info" onClick={props.changeView}>
+                    <div className="name-age">
+                        <h3>{props.displayedPerson.firstName}&nbsp;-</h3 >&nbsp;<h3>25</h3>
+                    </div>
+                    <div className="bio"><p>{shortBio}</p></div>
+                </div>}
             <Carousel interval={null} fade={true}>
-                {profilePictures}
+                
+                {props.displayedPerson ? 
+                props.displayedPerson.profilePictures.length > 0 ?
+                    showProfilePictures
+                    :
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={'http://localhost:3001/uploads/placeholder.jpg'}
+                            alt="First slider"
+                        />
+                    </Carousel.Item>
+               : ''}
             </Carousel>
         </div>
     )
