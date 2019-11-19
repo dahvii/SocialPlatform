6676a0ff063770faef7a6ffdfefea0ed25f80af5
Eric Rasmusson Lindström <eric.rl@me.com>
2019-11-19 16:02:32 +0100

    removed some clg

--- social-platform/backend/src/api/index.js
+++ social-platform/backend/src/api/index.js
@@ -217,7 +217,6 @@ router.get('/api/feed-post/:id', async (req, res) => {
 });
 
 router.get('/api/questions/:skip', async (req, res) => {
-    console.log("req.params.skip: ", req.params.skip)
     let result = await dbModels['questions']
         .find()
         .skip(parseInt(req.params.skip))
@@ -300,7 +299,6 @@ router.put('/api/user-question/setAnswered', async (req, res) => {
     let currentUser = await dbModels['user'].findOne({ _id: req.body.userId })
     currentUser.questionsAnswered += req.body.questionsAnswered
     currentUser.save()
-    console.log("i setAnswered: ", currentUser.questionsAnswered)
     res.json(currentUser)
 })
 
