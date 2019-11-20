195582d47a5baf46b033a8dac540bcffd16f01c9
Elliott <dahviimakes@gmail.com>
2019-11-20 15:43:45 +0100

    add button

--- social-platform/frontend/src/views/MyProfile.js
+++ social-platform/frontend/src/views/MyProfile.js
@@ -11,6 +11,8 @@ export default function MyProfile(props) {
     useLifeCycle({
         mount: () => {
             setAge(calcAge(state.currentUser.dateOfBirth))
+            console.log(state.currentUser.admin);
+
         }
     })
     const logout = async () => {
@@ -28,6 +30,10 @@ export default function MyProfile(props) {
         props.history.push(`/profile/${state.currentUser.id}`)
     }
 
+    const goToAdmin = () => {
+        props.history.push(`/adminPage`)
+    }
+
     const goToEditProfile = () => {
         props.history.push('/edit-profile')
     }
@@ -36,12 +42,15 @@ export default function MyProfile(props) {
         <div className="myprofile-content">
             <Image src={state.currentUser.profilePictures[0] ? `http://localhost:3001/${state.currentUser.profilePictures[0]}` : 'http://localhost:3001/uploads/placeholder.jpg'} alt="profile-picture" roundedCircle className="profile-picture" />
             <div className="myprofile-info">
-    <h3>{state.currentUser.firstName} -</h3>&nbsp;<h3>{age}</h3>
+                <h3>{state.currentUser.firstName} -</h3>&nbsp;<h3>{age}</h3>
             </div>
             <div className="myprofile-buttons">
                 <Button className="profile-button" variant="light" onClick={goToProfile}>Visa profil<i className="far fa-eye"></i></Button>
                 <Button className="profile-button" variant="light" onClick={goToEditProfile}>Redigera profil<i className="fas fa-cog"></i></Button>
             </div>
+            {state.currentUser.admin &&
+                <Button className="profile-button" variant="light" onClick={goToAdmin}>Gå till admin-sidan</Button>
+            }
             <div className="myprofile-logout">
                 <Button variant="light" className="myprofile-logout-button" onClick={logout}>Logga ut</Button>
             </div>
